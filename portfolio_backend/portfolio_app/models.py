2a73d83d022bd2994f890f6722377c3e92667130
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-14 16:28:47 +0000

    second_commit

--- portfolio_backend/portfolio_app/models.py
+++ portfolio_backend/portfolio_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    link = models.URLField(blank=True)
+    def __str__(self):
+        return self.title
--- portfolio_backend/portfolio_app/views.py
+++ portfolio_backend/portfolio_app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Project
 
-# Create your views here.
+def home(request):
+    data = {
+        'name':'Sagar Bhalerao',
+        'tagline' : 'Softwere Developer',
+        'contact_links' : {
+            'email':'sagar.bhalerao@yahoo.co.in',
+            'github':'sa-ga-r/github.com'
+        }
+    }
+    return JsonResponse(data)
+
+def about(request):
+    data = {
+        'bio' : 'I am a passionate developer with knowledge of Python, Django, javascript and react.',
+        'skills' : ["Python", "Django", "Javascript", "React"],
+    }
+    return JsonResponse(data)
+
+def projects(request):
+    projects = Project.objects.all().values("title", "description", "links")
+    return JsonResponse(list(projects), safe=False)
+
+def contacts(request):
+    data = {
+        'email' : 'sagar.bhalerao@yahoo.co.in',
+        'socials' : {
+            'twitter' : 'twitter.com/sagarbhalerao',
+            'linkedin' : 'linkedin.com/sagarbhalerao',
+        }
+    }
+    return JsonResponse(data)
