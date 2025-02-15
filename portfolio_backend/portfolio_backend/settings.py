8ca6936c17f906667d2c2e4e814514ac61c8448b
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-15 16:11:12 +0000

    sixth commit

--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "silk",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "silk.middlewere.SilkyMiddleware",
 ]
 
 ROOT_URLCONF = "portfolio_backend.urls"
