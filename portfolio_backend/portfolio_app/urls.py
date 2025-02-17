17798b2d9bfb7d941247003be509e79b1d897e5e
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-17 09:48:53 +0000

    update

--- portfolio_backend/portfolio_app/urls.py
+++ portfolio_backend/portfolio_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("home/", views.home, name='home')
+    path("home/", views.home, name='home'),
     path("about/", views.about, name='about'),
     path("projects/", views.projects, name='projects'),
     path("contacts/", views.contacts, name='contacts'),
